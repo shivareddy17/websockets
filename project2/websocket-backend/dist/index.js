98102c1a35b63685d29f6270e4410a4656b9bfd3
shivareddy17 <shivareddych17@gmail.com>
2025-02-06 17:42:03 +0530

    added gitignore and some functionalities to websocket

--- project2/websocket-backend/dist/index.js
+++ project2/websocket-backend/dist/index.js
@@ -4,16 +4,43 @@ const ws_1 = require("ws");
 const ws = new ws_1.WebSocketServer({ port: 3021 });
 let users = 0;
 let arr = [];
-ws.on("connection", (socket) => {
-    arr.push(socket);
-    users++;
-    console.log("connected" + users);
-    socket.send("sent by server" + users);
-    socket.on('message', (e) => {
-        for (let i = 0; i < arr.length; i++) {
-            const s = arr[i];
-            s.send(e.toString() + " Sent by user");
-        }
-        // socket.send(e.toString()+ "Sent by user")
+try {
+    ws.on("connection", (socket) => {
+        //arr.push(socket)
+        users = arr.length + 1;
+        //console.log("connected"+users)
+        // socket.send("sent by server"+users)
+        socket.on('message', (message) => {
+            var _a;
+            const parsedMessage = JSON.parse(message);
+            if (parsedMessage.type === "join") {
+                console.log("Joining room" + parsedMessage.payload.roomId);
+                arr.push({
+                    socket,
+                    room: parsedMessage.payload.roomId
+                });
+            }
+            if (parsedMessage.type === "chat") {
+                let currentRoom = (_a = arr.find((x) => x.socket === socket)) === null || _a === void 0 ? void 0 : _a.room;
+                for (let i = 0; i < arr.length; i++) {
+                    if (arr[i].room === currentRoom) {
+                        arr[i].socket.send(parsedMessage.payload.message);
+                    }
+                }
+            }
+            // for(let i=0;i<arr.length;i++){
+            //     const s=arr[i]
+            //     s.send(e.toString()+ " Sent by user")
+            // }
+            // socket.send(e.toString()+ "Sent by user")
+        });
+        socket.on("close", () => {
+            arr = arr.filter((x) => x.socket !== socket);
+            console.log("Client disconnected");
+        });
+        console.log("connected" + users);
     });
-});
+}
+catch (e) {
+    console.log(e);
+}
--- project2/websocket-backend/src/index.ts
+++ project2/websocket-backend/src/index.ts
@@ -2,21 +2,55 @@ import { WebSocketServer,WebSocket } from "ws";
 
 const ws =new WebSocketServer({port:3021});
 let users=0;
-let arr=[]
+interface user{
+    socket:WebSocket,
+    room:string
+}
 
+let arr:user[]=[]
+try{
 ws.on("connection",(socket)=>{
-    arr.push(socket)
-    users++;
-    console.log("connected"+users)
+    //arr.push(socket)
+    users=arr.length+1;
+    //console.log("connected"+users)
 
 
-    socket.send("sent by server"+users)
+   // socket.send("sent by server"+users)
 
-    socket.on('message',(e)=>{
-        for(let i=0;i<arr.length;i++){
-            const s=arr[i]
-            s.send(e.toString()+ " Sent by user")
+    socket.on('message',(message)=>{
+        const parsedMessage=JSON.parse(message as unknown as string)
+        if(parsedMessage.type==="join"){
+            console.log("Joining room"+parsedMessage.payload.roomId)
+            arr.push({
+                socket,
+                room:parsedMessage.payload.roomId
+            }
+            )
         }
+
+
+        if(parsedMessage.type==="chat"){
+            let currentRoom=arr.find((x)=>x.socket===socket)?.room
+
+            for(let i=0;i<arr.length;i++){
+                if(arr[i].room===currentRoom){
+                    arr[i].socket.send(parsedMessage.payload.message)
+                }
+            }
+        }
+        // for(let i=0;i<arr.length;i++){
+        //     const s=arr[i]
+        //     s.send(e.toString()+ " Sent by user")
+        // }
        // socket.send(e.toString()+ "Sent by user")
     })
-})
+    socket.on("close", () => {
+        arr = arr.filter((x) => x.socket !== socket);
+        console.log("Client disconnected");
+    });
+    
+    
+    console.log("connected"+users)
+})}catch(e){
+    console.log(e)
+}
